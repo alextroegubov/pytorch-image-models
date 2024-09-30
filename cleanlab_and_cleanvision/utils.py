c82aaf640b53c715254794fdf03eaf706ffd76c2
troegubov.aiu <troegubov.aiu@inviewlab.com>
2024-09-30 18:16:23 +0300

    add cleanlab utils

---
+++ cleanlab_and_cleanvision/utils.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+
+from sklearn import model_selection
+
+def write_filenames_to_file(save_dir, filename, names_lst):
+    with open(Path(save_dir) / filename, mode='w', encoding='utf-8') as f:
+        f.writelines(names_lst)
+
+
+def split_k_fold_files(root: str | Path, save_dir: str | Path, n_folds: int = 5,
+                       pattern: str = '*', seed: int = 0):
+    
+    filenames = list(map(lambda x: x.name, Path(root).rglob(pattern)))
+    k_folds = model_selection.KFold(n_splits=n_folds, shuffle=True, random_state=seed)
+
+    for fold_idx, (train_filenames, test_filenames) in enumerate(k_folds.split(filenames)):
+        write_filenames_to_file(save_dir, f'train_{fold_idx}_fold.txt', train_filenames)
+        write_filenames_to_file(save_dir, f'test_{fold_idx}_fold.txt', test_filenames)
+    
+
