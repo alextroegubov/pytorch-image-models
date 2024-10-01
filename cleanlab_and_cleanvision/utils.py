49af04699864c74279662d3727d4fcc69361feb7
troegubov <troegubov@wireless.iitp.ru>
2024-10-01 09:29:15 +0300

    add script draft for getting features and probabilities

--- cleanlab_and_cleanvision/utils.py
+++ cleanlab_and_cleanvision/utils.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 from sklearn import model_selection
 
-def write_filenames_to_file(save_dir, filename, names_lst):
+def write_filenames_to_file(save_dir: str | Path, filename: str, names_lst: list[str]):
     with open(Path(save_dir) / filename, mode='w', encoding='utf-8') as f:
         f.writelines(names_lst)
 
@@ -16,5 +16,13 @@ def split_k_fold_files(root: str | Path, save_dir: str | Path, n_folds: int = 5,
     for fold_idx, (train_filenames, test_filenames) in enumerate(k_folds.split(filenames)):
         write_filenames_to_file(save_dir, f'train_{fold_idx}_fold.txt', train_filenames)
         write_filenames_to_file(save_dir, f'test_{fold_idx}_fold.txt', test_filenames)
+
+
+def read_filenames_from_file(filename: str | Path) -> list[str]:
+    with open(filename, mode='r', encoding='utf-8') as f:
+        names = f.readlines()
     
+    return names
+
+
 
